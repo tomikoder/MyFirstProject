ad1cae9aaac6361c786380eb1d290dfaacc988dc
Tomek J <tommi_j@yahoo.com>
2021-03-24 19:52:00 +0100

    Initial commit

---
+++ myshoop/users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+CustomUser = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
